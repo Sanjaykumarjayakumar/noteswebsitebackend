a0d4ee392dfe1efe519f05e5562ad6d503595b1c
Sanjaykumarjayakumar <153343937+Sanjaykumarjayakumar@users.noreply.github.com>
2026-07-01 01:06:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 require("dotenv").config();
 const express = require("express");
-const cors =require("cors");
-const connectDB =require("./config/db");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
 connectDB();
+
 const app = express();
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use("/api/auth",require("./routes/authRoutes"));
-app.use("/api/notes",require("./routes/noteRoutes"));
-app.listen(process.env.PORT,()=>{
-    console.log("Server Running");
+
+const corsOptions = {
+  origin: "https://noteswebsite-nine.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/notes", require("./routes/noteRoutes"));
+
+app.listen(process.env.PORT, () => {
+  console.log("Server Running");
 });
